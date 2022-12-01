52402089de3b7e6b87d8ce881dafbcafb938e0df
Erik Hansbo <erik.hansbo@seb.se>
2022-12-01 09:48:38 +0100

    remove more parentheses

--- DayOne.hs
+++ DayOne.hs
@@ -4,6 +4,6 @@ import Data.List
 
 main :: IO ()
 main = do
-    elves <- (reverse . sort) <$> map (sum . map read . filter (/= "") . splitOn "\n")  <$> splitFile "day1" "\n\n"
+    elves <- reverse . sort . map (sum . map read . filter (/= "") . splitOn "\n") <$> splitFile "day1" "\n\n"
     print $ elves !! 0
     print $ sum $ take 3 elves
